e4ebd3ce8dc2d57382fce4601a4d94e5cb86b22c
5IHYUN <sini5971@gmail.com>
2025-05-28 07:15:57 +0000

    .

--- src/models/post.js
+++ src/models/post.js
@@ -333,6 +333,16 @@ async getComments(post_id) {
     return { success: false, msg: err.message || err };
   }
 }
+//start-service 요청 처리: postId, imgUrl 보내기
+  async getImagesInfo(university_id) {
+    try{
+      console.log('model/post.getImagesInfo 실행');
+      const response = await PostStorage.getImagesInfo(university_id);
+      return response;
+    } catch (err) {
+      return {success:false ,msg:err};
+    }
+  }
 }
         
 
