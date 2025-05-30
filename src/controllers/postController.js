73f36a60651a45d699afa016e007578a3383d9f4
youngseoOh <dudtj2379@gmail.com>
2025-05-31 03:34:28 +0900

    테스트용

--- src/controllers/postController.js
+++ src/controllers/postController.js
@@ -210,7 +210,7 @@ const postController = {
       // const user = await fetchUserInfoFromUserService(req.headers.cookie);
       // const user_email = user.user_email;
       // 테스트용 하드코딩
-      user_email = "student@sungshin.ac.kr";
+      const user_email = "student@sungshin.ac.kr";
       console.log("user_email");
       console.log(user_email);
       let response;
