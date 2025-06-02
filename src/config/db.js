6cd44be3fc6579f2a2533d2ae20ad8b23b88814f
hini5971 <sini5971@gmail.com>
2025-06-02 16:03:57 +0900

    db 환경변수 수정

--- src/config/db.js
+++ src/config/db.js
@@ -3,11 +3,11 @@ require("dotenv").config();
 
 const pool = mysql.createPool({
   connectionLimit: 10,
-  host: process.env.POST_SERVICE_DB_HOST,
-  user: process.env.POST_SERVICE_DB_USER,
-  password: process.env.POST_SERVICE_DB_PASSWORD,
-  database: process.env.POST_SERVICE_DB_NAME,
-  port: process.env.POST_SERVICE_DB_PORT,
+  host: process.env.DB_HOST,
+  user: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_NAME,
+  port: process.env.DB_PORT,
   charset: 'utf8mb4'
 });
 
