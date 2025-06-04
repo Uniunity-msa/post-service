69fd704da4325e2e75d4d306ce2c150bc0f94843
hini5971 <sini5971@gmail.com>
2025-06-04 14:19:33 +0900

    fix: mypage url수정

--- src/public/js/apiUrl.js
+++ src/public/js/apiUrl.js
@@ -3,6 +3,7 @@ export const apiUrl = 'http://uniunity.store';
 export const baseUrls = {
   post: `${apiUrl}/post`,
   reaction: `${apiUrl}/reaction`,
+  reaction2: `${apiUrl}/mypage`,
   user: `${apiUrl}/auth`,
   user2: `${apiUrl}/user`,
   start: `${apiUrl}/council`,
