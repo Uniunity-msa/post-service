415246247c4e163663eaa0ef90f20b549de2ca74
hini5971 <sini5971@gmail.com>
2025-06-06 01:49:18 +0900

    수정

--- src/public/js/post/postviewer.js
+++ src/public/js/post/postviewer.js
@@ -507,7 +507,7 @@ writeCommentBtn.addEventListener('click', function () {
 //게시글지우기
 const fetchDeletePost = async (post_id, user_email) => {
   try {
-    const response = await fetch(`/deletePost/${post_id}/${user_email}`, {
+    const response = await fetch(`${postApiUrl}/deletePost/${post_id}/${user_email}`, {
       method: 'DELETE',
       headers: {
         'Content-Type': 'application/json'
